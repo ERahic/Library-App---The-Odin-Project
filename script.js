f6055560666966672955df8bb71b28fe88e543b8
Edis Rahic <rahic.edis@gmail.com>
2025-07-01 17:20:58 -0400

    Adjusted grid layout for Books. Added a form that will allow user to input their book details. New book button will display book info form. cancel button inside of form will make the form hidden.

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 "use strict";
+
+// Library array that will hold book objects created by the user
+const myLibrary = [];
+
+// Book constructor to create book objects
+function Book(title, author, pages, read) {
+  // Constructor
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+// function to create books
+function addBookToLibrary() {
+  // take params, create a book and then store it in the array
+}
+
+//capturing divs with querySelector
+const form = document.querySelector(".form-container");
+
+// Btn to add new book via a form
+const addBookBtn = document.querySelector(".new-book-btn");
+addBookBtn.addEventListener("click", function () {
+  console.log("NEW BOOK ADDED");
+  form.classList.remove("hidden");
+});
+
+// Btn to remove books that are on display
+const deleteBookBtn = document.querySelector(".delete-book-btn");
+deleteBookBtn.addEventListener("click", function () {
+  console.log("BOOK DELETED");
+});
+
+// Btn to cancel the form submition
+const cancelFormBtn = document.querySelector(".cancel-btn");
+cancelFormBtn.addEventListener("click", function () {
+  console.log("FORM CANCEL CLICKED");
+  form.classList.add("hidden");
+});
