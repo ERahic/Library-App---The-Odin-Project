892eb360f58ce746771c3734b3708735f5e6e52a
Edis Rahic <rahic.edis@gmail.com>
2025-07-02 14:17:10 -0400

    Added a button for the library card example to demonstrate that the user can changed the read status of their book. Generated a new button for each book that is submitted by the user. Added an event listener to toggle the read status of the book the user had sumbitted on the form.

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ function addBookToLibrary() {
   const author = document.querySelector("#author").value;
   const pages = document.querySelector("#pages").value;
   const read = document.querySelector('input[name="read"]:checked').value;
+
   // Create new book and push it to the my library array
   const newBook = new Book(id, title, author, pages, read);
   myLibrary.push(newBook);
@@ -36,6 +37,7 @@ function addBookToLibrary() {
 function updateLibrary() {
   // CLEAR THE BOOK GRID OF ANYTHING IN ORDER TO PREVENT DUPLICATION
   bookGrid.innerHTML = "";
+
   // Loop through the array and generate div's for each book found
   myLibrary.forEach((book) => {
     // Create new library card and book info div's to display on webpage
@@ -45,28 +47,62 @@ function updateLibrary() {
     newBookInfo.classList.add("book-info");
     newBookInfo.innerHTML = `<h2>${book.title}</h2> <h3>Author: ${book.author}</h3> 
     <h3>Pages: ${book.pages}</h3> <h3>Has Read? ${book.read}</h3>`;
+
     // If the user read the book, left border will be green, else, red
     if (book.read === "Yes") {
       newLibraryCard.style.borderLeft = "solid 1rem green";
     } else {
       newLibraryCard.style.borderLeft = "solid 1rem red";
     }
+
+    // Btn to change the status of book read
+    const readBtn = document.createElement("button");
+    readBtn.innerText = "HAS READ?";
+    readBtn.classList.add("delete-book-btn");
+    readBtn.style.backgroundColor = "rgb(14, 165, 233)";
+    readBtn.dataset.id = book.id;
+
+    // When button is clicked, toggle the read status to either yes or no
+    readBtn.addEventListener("click", function () {
+      // match the readBtn's dataset with the book's id
+      const hasReadToggle = this.dataset.id;
+      // loop through each book in library array and check if the book id matches with the dataset of button
+      myLibrary.forEach((book) => {
+        if (book.id === hasReadToggle) {
+          if (book.read === "Yes") {
+            book.read = "No";
+            newLibraryCard.style.borderLeft = "solid 1rem red";
+          } else {
+            book.read = "Yes";
+            newLibraryCard.style.borderLeft = "solid 1rem green";
+          }
+        }
+        // re-render the page to update the read status for book selected
+        updateLibrary();
+      });
+    });
+
     // Btn to remove books that are on display
     const deleteBookBtn = document.createElement("button");
     deleteBookBtn.innerText = "DELETE BOOK";
     deleteBookBtn.classList.add("delete-book-btn");
+
     // Assigning book.id to delete button to set the data attribute
     deleteBookBtn.dataset.id = book.id;
+
     // Event listener to remove the book by matching the id of delete btn dataset
     deleteBookBtn.addEventListener("click", function () {
       const bookToDelete = this.dataset.id;
       myLibrary = myLibrary.filter((book) => book.id != bookToDelete);
       console.log("BOOK DELETED");
       console.log(bookToDelete);
+
+      // Re-render to update the library array and display the remaining books or show none if only one book was created
       updateLibrary();
     });
     bookGrid.appendChild(newLibraryCard);
     newLibraryCard.appendChild(newBookInfo);
+    newBookInfo.appendChild(readBtn);
     newBookInfo.appendChild(deleteBookBtn);
   });
 }
