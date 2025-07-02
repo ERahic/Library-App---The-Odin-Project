1b51dd533b121996b123cd32344053cbe855c951
Edis Rahic <rahic.edis@gmail.com>
2025-07-01 20:04:04 -0400

    Changed delete book button from header to inside the library card. Added funcitonality to add new books into library array. Added functionality to loop through library array and generate new library card div's. Fixed bug where books entered before would duplicate when entering a new book into the library array. Submit button inside of form will hide the form and clear it, as well as passing the values to addBookToLibrary function. Cancel button inside of form will hide the form itself and clear any data written in the input's.

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 "use strict";
+//capturing divs with querySelector
+const form = document.querySelector(".form-container");
+const newBookForm = document.querySelector("#new-book-form");
+const bookGrid = document.querySelector(".book-grid");
 
 // Library array that will hold book objects created by the user
 const myLibrary = [];
@@ -15,10 +19,41 @@ function Book(title, author, pages, read) {
 // function to create books
 function addBookToLibrary() {
   // take params, create a book and then store it in the array
+  const title = document.querySelector("#book_title").value;
+  const author = document.querySelector("#author").value;
+  const pages = document.querySelector("#pages").value;
+  const read = document.querySelector('input[name="read"]:checked').value;
+  // Create new book and push it to the my library array
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  console.log("NEW BOOK ADDED: ", newBook);
+  updateLibrary();
 }
 
-//capturing divs with querySelector
-const form = document.querySelector(".form-container");
+// funciton to loop through myLibrary array and generate new books on webpage
+function updateLibrary() {
+  // CLEAR THE BOOK GRID OF ANYTHING IN ORDER TO PREVENT DUPLICATION
+  bookGrid.innerHTML = "";
+  // Loop through the array and generate div's for each book found
+  myLibrary.forEach((book) => {
+    // Create new library card and book info div's to display on webpage
+    const newLibraryCard = document.createElement("div");
+    newLibraryCard.classList.add("library-card");
+    const newBookInfo = document.createElement("div");
+    newBookInfo.classList.add("book-info");
+    newBookInfo.innerHTML = `<h2>${book.title}</h2> <h3>Author: ${book.author}</h3> 
+    <h3>Pages: ${book.pages}</h3> <h3>Has Read? ${book.read}</h3> 
+    <button type="button" class="delete-book-btn">DELETE BOOK</button>`;
+    // If the user read the book, left border will be green, else, red
+    if (book.read === "Yes") {
+      newLibraryCard.style.borderLeft = "solid 1rem green";
+    } else {
+      newLibraryCard.style.borderLeft = "solid 1rem red";
+    }
+    bookGrid.appendChild(newLibraryCard);
+    newLibraryCard.appendChild(newBookInfo);
+  });
+}
 
 // Btn to add new book via a form
 const addBookBtn = document.querySelector(".new-book-btn");
@@ -38,4 +73,15 @@ const cancelFormBtn = document.querySelector(".cancel-btn");
 cancelFormBtn.addEventListener("click", function () {
   console.log("FORM CANCEL CLICKED");
   form.classList.add("hidden");
+  newBookForm.reset();
+});
+
+// Btn to submit the data entered by user
+const submitFormBtn = document.querySelector(".submit-btn");
+submitFormBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log("SUBMIT BUTTON CLICKED");
+  form.classList.add("hidden");
+  addBookToLibrary();
+  newBookForm.reset();
 });
